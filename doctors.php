b4deb815b84c18fdfb971add421edb4dab6e4f05
ben-hapip <benhapip@gmail.com>
2016-11-28 22:04:31 -0600

    Add files via upload

---
+++ doctors.php
@@ -0,0 +1,22 @@
+<?php
+
+require("common_funcs.php");
+
+$cursor = new page_header("Cloud9 Pharma");
+$cursor->add_stylesheet("style_index.css");
+$cursor->add_stylesheet("https://fonts.googleapis.com/css?family=Poiret+One");
+$cursor->export();
+
+$cursor = new jumbotron("img/cloud9.png");
+$cursor->export();
+
+standard_nav();
+
+?>
+
+<?php
+
+$cursor = new page_footer("Cloud 9 Pharma");
+$cursor->export();
+
+?>
